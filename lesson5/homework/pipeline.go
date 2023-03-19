29027a50238d33a9bc831d9c19eec10c96399033
Denis <denok629@gmail.com>
2023-03-19 10:55:33 +0300

    sync recursion method done

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -12,6 +12,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	channels := make([]In, len(stages)+1)
+	res := make(chan any)
+
+	for i := range channels {
+		channels[i] = make(In)
+	}
+	channels[0] = in
+
+	for i := 0; i < len(stages); i++ {
+		channels[i+1] = stages[i](channels[i])
+	}
+
+	go func() {
+		defer close(res)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case data, ok := <-channels[len(stages)]:
+				if !ok {
+					return
+				}
+				res <- data
+			}
+		}
+	}()
+	return res
 }
