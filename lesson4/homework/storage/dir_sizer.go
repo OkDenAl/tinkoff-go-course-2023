87909b1eb3417747f94f7d32c6cf5780cd8b9d6b
Denis <denok629@gmail.com>
2023-03-20 00:21:33 +0300

    all tests passed

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -55,26 +55,38 @@ func startWorker(ctx context.Context, in <-chan Dir, errch chan<- error, done ch
 }
 
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	res := Result{Size: 0, Count: 0}
-	dirs, files, err := d.Ls(ctx)
-	if err != nil {
-		return Result{}, err
+	res := Result{}
+	workerInput := make(chan Dir)
+	errorCh := make(chan error)
+	done := make(chan struct{})
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < a.maxWorkersCount; i++ {
+		wg.Add(1)
+		go startWorker(ctx, workerInput, errorCh, done, &wg, &res)
 	}
-	res.Count += int64(len(files))
-	for _, file := range files {
-		size, err := file.Stat(ctx)
-		if err != nil {
-			return Result{}, err
+
+	que := make([]Dir, 0) // queue ds
+	que = append(que, d)
+	workerInput <- d
+	for {
+		if len(done) == 0 && len(que) == 0 {
+			close(workerInput)
+			break
 		}
-		res.Size += size
-	}
-	for _, dir := range dirs {
-		p, err := a.Size(ctx, dir)
-		if err != nil {
+		select {
+		case err := <-errorCh:
 			return Result{}, err
+		case <-done:
+			curDir := que[0]
+			que = que[1:]
+			dirs, _, _ := curDir.Ls(ctx)
+			for _, dir := range dirs {
+				que = append(que, dir)
+				workerInput <- dir
+			}
 		}
-		res.Size += p.Size
-		res.Count += p.Count
 	}
+	wg.Wait()
 	return res, nil
 }
