2c000cc0c87ec6b70860ea41cfe5b1d5d4166ec1
Denis <denok629@gmail.com>
2023-03-24 14:08:04 +0300

    all tests have passed

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -2,6 +2,7 @@ package executor
 
 import (
 	"context"
+	"sync"
 )
 
 type (
@@ -12,6 +13,46 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	var (
+		wg      = sync.WaitGroup{}
+		inputs  = make([]chan any, 0)
+		outputs = make([]Out, 0)
+		res     = make(chan any)
+		mu      = sync.Mutex{}
+	)
+	j := 0
+
+	for a := range in {
+		inputs = append(inputs, make(chan any))
+		outputs = append(outputs, make(chan any))
+
+		wg.Add(1)
+
+		go func(input In, j int) {
+			defer wg.Done()
+			for i := 0; i < len(stages); i++ {
+				outputs[j] = stages[i](input)
+				input = outputs[j]
+			}
+		}(inputs[j], j)
+
+		select {
+		case <-ctx.Done():
+			close(res)
+			return res
+		default:
+			inputs[j] <- a
+			close(inputs[j])
+			j++
+		}
+	}
+
+	wg.Wait()
+	go func() {
+		for _, ch := range outputs {
+			res <- <-ch
+		}
+		close(res)
+	}()
+	return res
 }
