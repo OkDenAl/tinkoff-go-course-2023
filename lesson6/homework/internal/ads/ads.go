387117b3f625c1ef4b51a47a83f5ad67c65d9860
Denis <denok629@gmail.com>
2023-04-05 22:07:44 +0300

    hw6 done

--- lesson6/homework/internal/ads/ads.go
+++ lesson6/homework/internal/ads/ads.go
@@ -1,9 +1,36 @@
 package ads
 
+import (
+	"errors"
+	"github.com/OkDenAl/validator"
+)
+
+var (
+	ErrUserCantChangeThisAd = errors.New("the user is trying to change an ad created by another user")
+	ErrInvalidAdParams      = errors.New("invalid ad params")
+)
+
 type Ad struct {
 	ID        int64
 	Title     string
 	Text      string
 	AuthorID  int64
 	Published bool
+}
+
+type ValidatorAd struct {
+	TitleMin string `validate:"min:1"`
+	TitleMax string `validate:"max:100"`
+	TextMin  string `validate:"min:1"`
+	TextMax  string `validate:"max:500"`
+}
+
+func ValidateAd(ad *Ad) error {
+	vAd := ValidatorAd{
+		TitleMin: ad.Title,
+		TitleMax: ad.Title,
+		TextMin:  ad.Text,
+		TextMax:  ad.Text,
+	}
+	return validator.Validate(vAd)
 }
