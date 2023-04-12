1bf44cbb5ed10297cf9a600c90d5f9d53f766628
Denis <denok629@gmail.com>
2023-04-12 10:48:16 +0300

    prepare code from hw6

---
+++ lesson8/homework/internal/adapters/adrepo/repo.go
@@ -0,0 +1,38 @@
+package adrepo
+
+import (
+	"context"
+	"homework8/internal/ads"
+	"homework8/internal/app"
+)
+
+type repository struct {
+	data           map[int64]*ads.Ad
+	curIdGenerator int64
+}
+
+func New() app.Repository {
+	return &repository{data: make(map[int64]*ads.Ad), curIdGenerator: 0}
+}
+
+func (r *repository) AddAd(ctx context.Context, ad *ads.Ad) (int64, error) {
+	ad.ID = r.curIdGenerator
+	r.data[r.curIdGenerator] = ad
+	r.curIdGenerator++
+	return ad.ID, nil
+}
+
+func (r *repository) GetAd(ctx context.Context, adId int64) (*ads.Ad, error) {
+	return r.data[adId], nil
+}
+
+func (r *repository) UpdateAdStatus(ctx context.Context, adId int64, newStatus bool) (*ads.Ad, error) {
+	r.data[adId].Published = newStatus
+	return r.data[adId], nil
+}
+
+func (r *repository) UpdateAdTitleAndText(ctx context.Context, adId int64, newTitle, newText string) (*ads.Ad, error) {
+	r.data[adId].Text = newText
+	r.data[adId].Title = newTitle
+	return r.data[adId], nil
+}
---
+++ lesson8/homework/internal/ports/httpfiber/handlers.go
@@ -0,0 +1,93 @@
+package httpfiber
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"homework8/internal/ads"
+	"homework8/internal/app"
+	"net/http"
+)
+
+func createAd(a app.App) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var reqBody createAdRequest
+		err := c.BodyParser(&reqBody)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		ad, err := a.CreateAd(c.Context(), reqBody.Title, reqBody.Text, reqBody.UserID)
+		if err != nil {
+			switch err {
+			case ads.ErrInvalidAdParams:
+				c.Status(http.StatusBadRequest)
+			default:
+				c.Status(http.StatusInternalServerError)
+			}
+			return c.JSON(AdErrorResponse(err))
+		}
+		return c.JSON(AdSuccessResponse(ad))
+	}
+}
+
+// Метод для изменения статуса объявления (опубликовано - Published = true или снято с публикации Published = false)
+func changeAdStatus(a app.App) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var reqBody changeAdStatusRequest
+		if err := c.BodyParser(&reqBody); err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		adID, err := c.ParamsInt("ad_id")
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		ad, err := a.ChangeAdStatus(c.Context(), int64(adID), reqBody.UserID, reqBody.Published)
+		if err != nil {
+			switch err {
+			case ads.ErrUserCantChangeThisAd:
+				c.Status(http.StatusForbidden)
+			default:
+				c.Status(http.StatusInternalServerError)
+			}
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		return c.JSON(AdSuccessResponse(ad))
+	}
+}
+
+// Метод для обновления текста(Text) или заголовка(Title) объявления
+func updateAd(a app.App) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var reqBody updateAdRequest
+		if err := c.BodyParser(&reqBody); err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		adID, err := c.ParamsInt("ad_id")
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		ad, err := a.UpdateAd(c.Context(), int64(adID), reqBody.UserID, reqBody.Title, reqBody.Text)
+		if err != nil {
+			switch err {
+			case ads.ErrUserCantChangeThisAd:
+				c.Status(http.StatusForbidden)
+			case ads.ErrInvalidAdParams:
+				c.Status(http.StatusBadRequest)
+			default:
+				c.Status(http.StatusInternalServerError)
+			}
+			return c.JSON(AdErrorResponse(err))
+		}
+
+		return c.JSON(AdSuccessResponse(ad))
+	}
+}
