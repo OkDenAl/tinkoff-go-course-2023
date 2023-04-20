bdcfaf34d8acbb71638d9c608e750321c92d1587
Denis <denok629@gmail.com>
2023-04-20 20:49:17 +0300

    some fix

--- lesson9/homework/cmd/main/main.go
+++ lesson9/homework/cmd/main/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "homework9/internal/ports/httpgin"
+import (
+	"homework9/internal/adapters/adrepo"
+	"homework9/internal/adapters/userrepo"
+	"homework9/internal/app/adsapp"
+	"homework9/internal/app/userapp"
+	"homework9/internal/ports/httpgin"
+)
 
 func main() {
 	userRepo := userrepo.New()
-	server := httpgin.NewHTTPServer(":18080", adapp.NewApp(adrepo.New(), userRepo), userapp.NewApp(userRepo))
+	server := httpgin.NewHTTPServer(":18080", adsapp.NewApp(adrepo.New(), userRepo), userapp.NewApp(userRepo))
 	err := server.Listen()
 	if err != nil {
 		panic(err)
